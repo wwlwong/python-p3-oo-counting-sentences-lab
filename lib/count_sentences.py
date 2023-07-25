2856c6ef2f0fc2476c8b97158caea10474289a04
William Wong <william.wai.lim.wong@gmail.com>
2023-07-25 13:11:19 -0400

    passes test

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=""):
+    
+    self.value = value
+  
+  def get_value(self):
+    return self._value
+
+  def set_value(self, value):
+    if type(value) is str:
+      self._value = value
+    else:
+      print ("The value must be a string.")
+
+  value = property(get_value, set_value)
+
+  def is_sentence(self):
+    return self.value.endswith(".")
+
+  def is_question(self):
+    return self.value.endswith("?")
+
+  def is_exclamation(self):
+    return self.value.endswith("!")
+
+  def count_sentences(self):
+    import re
+    sentence_list = re.split(r'[!\?\.](?= )', self.value)  
+    while("" in sentence_list):
+      sentence_list.remove("")
+
+    return len(sentence_list)
+    
+  
+
+
